6ce0b32af81f810b9c7cfbfe7240f265d79a4ecf
N.Anilkumar <123154239+Nakulurianilkumar2004@users.noreply.github.com>
2025-05-28 15:33:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
-from bson.objectid import ObjectId
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -38,4 +38,6 @@ def store_user_details():
         return jsonify({'error': 'Internal Server Error'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))  # get port from env or default 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
+
